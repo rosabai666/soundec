8c5f71778ddf1b04ab357b50c2679f7d0f4ed732
Rosa <rosabai0430@gmail.com>
2023-04-17 12:31:33 +0800

    0417 整理 解决方案 和 产品 文件夹及其内容

---
+++ 2023/02产品/SNC8x/01)竞品分析/00)竞品芯片分析/[DSP-3]AudioProcessor/YDA/YDA174 DSE_Mode2018/YDA174 DSE_Mode_v20000(e)20170913(3D等DSP DSE模式）/MCUProgram/Sample/dsp_control.h
@@ -0,0 +1,93 @@
+<<<<<<< HEAD
+/* 	CONFIDENTIAL
+*********************************************************************************
+
+	File:			dsp_control.h
+	Version:		1.00.00
+	Description:	DSP Control Processing Header File
+
+	Copyright(c) 2012 Yamaha Corporation
+
+*********************************************************************************/
+
+/* proto type declaration */
+void Mute(void);
+void MuteRelease(void);
+void SetVolume(UC bVolID);
+
+void SetBalance(signed char bBalID);
+
+void SetCrossover(UC bNetworkID, UC *pbData);
+
+void SetS3D(UC *pbData);
+void S3DOn(bool bOn);
+
+void SetHXT(UC *pbData);
+void HXTOn(bool bOn);
+
+void SetAEQ(UC *pbData0, UC *pbData1);
+void AEQOn(bool bOn);
+
+void PresetLoudnessSetting(UC bEffectID);
+void UserLoudnessSetting(UC *pbData);
+void LoudnessOn(bool bOn);
+
+void SetPowerLimit(UC *pbData);
+void PowerLimitOn(bool bOn);
+
+void SetSoftClip(UC *pbData);
+void SoftClipOn(bool bOn);
+
+void SmallSignalDetectionOn(UC *pbData);
+void SmallSignalDetectionOff(void);
+
+void PlayFM(UC bContentID);
+
+/* end of file */
+=======
+/* 	CONFIDENTIAL
+*********************************************************************************
+
+	File:			dsp_control.h
+	Version:		1.00.00
+	Description:	DSP Control Processing Header File
+
+	Copyright(c) 2012 Yamaha Corporation
+
+*********************************************************************************/
+
+/* proto type declaration */
+void Mute(void);
+void MuteRelease(void);
+void SetVolume(UC bVolID);
+
+void SetBalance(signed char bBalID);
+
+void SetCrossover(UC bNetworkID, UC *pbData);
+
+void SetS3D(UC *pbData);
+void S3DOn(bool bOn);
+
+void SetHXT(UC *pbData);
+void HXTOn(bool bOn);
+
+void SetAEQ(UC *pbData0, UC *pbData1);
+void AEQOn(bool bOn);
+
+void PresetLoudnessSetting(UC bEffectID);
+void UserLoudnessSetting(UC *pbData);
+void LoudnessOn(bool bOn);
+
+void SetPowerLimit(UC *pbData);
+void PowerLimitOn(bool bOn);
+
+void SetSoftClip(UC *pbData);
+void SoftClipOn(bool bOn);
+
+void SmallSignalDetectionOn(UC *pbData);
+void SmallSignalDetectionOff(void);
+
+void PlayFM(UC bContentID);
+
+/* end of file */
+>>>>>>> origin
